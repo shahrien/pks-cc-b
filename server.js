008f33ef16bdf2649b33bc121c86f4074787b482
shahrien <shahrin@itools.com.my>
2023-11-30 22:52:36 +0800

    change port

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ dotenv.config();
 
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 443;
 
 cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
